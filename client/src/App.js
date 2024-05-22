d8c32b8cd2a47e07c3f0e36c4b5aa89ca72e79a5
dominikaglow <dominika01.glow@gmail.com>
2024-05-22 21:59:03 +0200

    Created client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Products from './components/Products';
-import Payment from './components/Payments';
-import Cart from './components/Cart';
-import { CartProvider } from './CartContext';
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/products" exact Component={Products} />
-            <Route path="/payments" exact Component={Payment} />
-            <Route path="/cart" exact Component={Cart} />
-          </Routes>
-        </div>
-      </Router>
-    </CartProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
