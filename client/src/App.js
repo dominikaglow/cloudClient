9b76c5393220cd3f7ad69d75bbe0117e3bd11a29
dominikaglow <dominika01.glow@gmail.com>
2024-05-22 22:21:10 +0200

    Added route to products

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Products from './components/Products';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+        <div className="App">
+          <div className='links'>
+            <Link to="/products">Products</Link>
+          </div>
+          <Routes>
+            <Route path="/products" exact Component={Products} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
